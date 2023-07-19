faa109ff89c759cc43e08b91f9e5f2eb01ece256
MuertoParado <nicolas.casanova20@gmail.com>
2023-07-19 15:42:13 -0500

    feat: :sparkles: guardar en cookie y adicional ya recibe la data get por body

--- routers/routes.js
+++ routers/routes.js
@@ -3,19 +3,19 @@ import { SignJWT, jwtVerify } from "jose";
 const app = Router();
 
 
-app.get("/:id/:nombre", async (req,res)=>{
-    let json={
-        id: req.params.id,
-        nombre: req.params.nombre
-    }
+app.get("/", async (req,res)=>{
+    let json=JSON.stringify(req.body)
 
     const encoder = new TextEncoder();
     const jwtconstructor = new SignJWT({json});
+    
     const jwt = await jwtconstructor
     .setProtectedHeader({alg:"HS256",typ:"JWT"})
     .setIssuedAt()
-    .setExpirationTime("30s")
+    .setExpirationTime("1h")
     .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+    res.cookie('data',jwt,{httpOnly: true});
+    console.log(`data: ${jwt}`);
     res.send(({jwt}));
 });
 
@@ -28,6 +28,7 @@ app.post('/',async (req,res)=>{
             authorization,
             encoder.encode(process.env.JWT_PRIVATE_KEY)
         );
+        console.log(jwtData);
         res.send(jwtData);
     }catch(error){
         res.status(401).send({message:"error tiempo :("});
